2a3d9fe1d3ed83fb66b842f1b6e6c952dcb86875
wizardp-park <wizardp80@gmail.com>
2013-10-15 08:52:25 +0900

    ll

--- 0.01/public/script/view/about/aboutView.js
+++ 0.01/public/script/view/about/aboutView.js
@@ -13,6 +13,27 @@ define(
 		el : 'div#contentsView',
 		render: function() {
         $(this.el).html(template());
+        $("#test").click(function(){
+        	$.ajax({
+        		type:"GET",
+        		url:"bmdb/1",
+        		data:{name:"1111", age:"2222"}
+        	}).done(function(data){
+        		console.log(data);
+        	}).fail(function(data){
+				console.log(data);
+        	});
+        	/*
+    		$.get( "bmdb/1", function( data ) {
+				alert('success!!');
+				console.log(data);
+			})
+			.fail(function(data){
+				alert('fail!!');
+				console.log(data);
+			});
+*/
+        });
         return this;
     }
 	});
